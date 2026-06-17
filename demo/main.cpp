f848c7073a3b0400b4ca5cdfd7845a49d6a54e22
アクゼスティア <akzestia@gmail.com>
2026-06-17 13:12:30 +0900

    Update demo

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,7 +1,44 @@
 #include <print>
 #include <tokyo/tokyo_main.hxx>
+#include <tokyo/tokyo_types.hxx>
+
+/// ^^
+fn parse(sv s) -> Result<i32, String> {
+    if (s.empty())
+        _ Err(String{"empty"});
+    _ Ok(5);
+}
+
+struct User {
+    str name;
+};
+
+fn user_exists(str name) -> bool {
+    if (name == "アクゼスチィア")
+        _ true;
+    _ false;
+}
+
+fn get_user(str name) -> Option<User> {
+    if (user_exists(name))
+        _ Some(User{
+            .name = name,
+        });
+    _ None;
+}
+
+struct DbPtr {};
 
 tokyo {
-    std::println("東京へよこそ！ ^~^");
+    let vec = Vec<u8>{1, 2, 3, 4};
+
+    Box<DbPtr> db = Box_new(DbPtr{});
+
+    let user = get_user("アクゼスチィア");
+
+    if (user) {
+        std::println("[{}] > 東京へよこそ！ ^~^", user->name);
+    }
+
     _ 0;
 }
